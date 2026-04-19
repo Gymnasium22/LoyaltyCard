5dbb3802317cb1716dfa78571650d0aea42e49fb
Nikita <stasevichnikita050@gmail.com>
2026-04-19 17:00:33 +0300

    Add basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/LoyaltyCard">
       <AppProvider>
         <Routes>
           <Route path="/" element={<HomePage />} />
